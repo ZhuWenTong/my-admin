cc92423d9ee0911cd3f98b9248eae108603f311c
zwt <893850280@qq.com>
2020-07-04 14:33:58 +0800

    主题中心

--- src/pages/Index/index.vue
+++ src/pages/Index/index.vue
@@ -5,7 +5,9 @@
         </el-menu>
         <div class="action-bar">
             <el-tooltip effect="dark" content="更换主题" placement="top">
-                <i class="fa fa-diamond" aria-hidden="true" @click="visible=true"></i>
+                <div @click="visible=true">
+                    <i class="fa fa-diamond" aria-hidden="true"></i>
+                </div>
             </el-tooltip>
         </div>
         <router-view></router-view>
@@ -53,13 +55,28 @@ export default {
     position: relative;
     .action-bar {
         padding: 0 20px;
-        line-height: 60px;
+        height: 60px;
         position: absolute;
         right: 0;
         top: 0;
-        i {
-            color: #909399;
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-start;
+        align-items: center;
+        div {
+            width: 40px;
+            height: 40px;
+            text-align: center;
+            line-height: 40px;
             cursor: pointer;
+            border: 1px solid transparent;
+            border-radius: 4px;
+            &:hover {
+                border-color: #EBEEF5;
+            }
+            i {
+                color: #909399;
+            }
         }
     }
 }
